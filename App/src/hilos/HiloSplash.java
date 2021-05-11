db0c5c57a292ecb1afca6c63b8fd6b7f84385d81
ProgrammerAuditore <victorvj098@gmail.com>
2021-05-10 21:42:18 -0500

    *** Avance : Se modifico el splash de bienvenida
    > .
    > Se agrego un tiempo de espara por cada recurso a cargar
    > Se creo codigo para cargar los datos para la conexion al servidor

--- App/src/hilos/HiloSplash.java
+++ App/src/hilos/HiloSplash.java
@@ -2,6 +2,8 @@ package hilos;
 
 import controlador.CtrlHiloConexion;
 import modelo.dao.ConexionDao;
+import modelo.dto.ConexionDto;
+import src.Source;
 import vista.splash.Bienvenida;
 
 public class HiloSplash extends Thread{
@@ -16,22 +18,29 @@ public class HiloSplash extends Thread{
         String[] msg = new String[3];
         int src = 2, avance = 0;
         
-        CtrlHiloConexion.ctrlDatos = new ConexionDao().obtener_conexion();
-        
+        Source.dataConexion.exists();
         for (int i = 1; i > 0; i--) {
             
-            splash.etqMensaje.setText("Estableciendo conexion..");
-            if(CtrlHiloConexion.mtdEstablecer()){
-                splash.etqMensaje.setText("Conexion establecienda.");
+            splash.etqMensaje.setText("Cargando datos de conexion..");
+            if( new ConexionDao().obtener_conexion() != null ){
+                splash.etqMensaje.setText("Datos de conexión cargado.");
+                System.out.println("Datos de conexión cargado. [!]");
             }else {
-                splash.etqMensaje.setText("Conexion no establecida.");
+                ConexionDto conec = new ConexionDto("0", "", "", "", "");
+                new ConexionDao().regitrar_conexion(conec);
+                splash.etqMensaje.setText("Datos de conexión creado.");
+                System.out.println("Datos de conexión creado. [!]");
             }
             
             avance += (100 / src);
             splash.pbProgreso.setValue(avance);
             splash.etqCarga.setText("" + avance + "%");
+            try {
+                Thread.sleep( avance * 10 );
+            } catch (Exception e) {}
         }
         
+        CtrlHiloConexion.ctrlDatos = new ConexionDao().obtener_conexion();
         for (int i = 1; i > 0; i--) {
             
             splash.etqMensaje.setText("Estableciendo conexion..");
@@ -44,6 +53,14 @@ public class HiloSplash extends Thread{
             avance += (100 / src);
             splash.pbProgreso.setValue(avance);
             splash.etqCarga.setText("" + avance + "%");
+            try {
+                Thread.sleep( avance * 2 );
+            } catch (Exception e) {}
+        }
+        
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
         }
         
         splash.setVisible(false);
