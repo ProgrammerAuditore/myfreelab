9b6d8a2b4fc75986fc1ed25916e41d977fcf4fb3
ProgrammerAuditore <victorvj098@gmail.com>
2021-06-20 02:06:29 -0500

    *** Avance: MyFreeLab v0.9.30Alpha > MyFreeLab v0.9.60Alpha

--- MyFreeLab/src/src/Info.java
+++ MyFreeLab/src/src/Info.java
@@ -8,23 +8,30 @@ public class Info {
     public static final String Descripcion = "Gestión de proyectos para freelancer.";
     public static final String Detalle = 
             "El programa sirve para poder crear, modificar, eliminar y registrar proyectos desarrollados para una empresa en particular." +"\n"+
-            "Asi también, para poder generar una cotizacion para cada empresa en particular y calcular el costo del proyecto." +"\n"+
+            "Asi también, para poder generar una cotizacion para cada empresa en particular y calcular el costo del proyecto, , entre otras cosas." +"\n"+
             "Tiene la funcionalidad de conexion a una base de datos MYSQL local o remoto para registrar los datos.";
     public static final String Licencia="Licencia GPL.";
     public static final String sNombre = "MyFreeLab";
-    public static final String sVersionNum = "0930";
-    public static final String sVersionName = "v0.9.30";
+    public static final String sVersionNum = "0960";
+    public static final String sVersionName = "v0.9.60";
     public static final String sProduccion = "Alpha";
     public static final String NombreSoftware = sNombre + " " + sVersionName + sProduccion;
     public static final String Novedades = 
-            "* Ya disponible .deb para linux desde la pagina oficial" + "\n" +
-            "* Ya disponible .exe para windows desde la pagina oficial" + "\n" +
-            "* Nuevo diseño para el menu principal" + "\n" +
-            "* Se agrego un nuevo barra de estado" + "\n" +
-            "* Se agrego la funcionalidad de generar cotizacion" + "\n" +
-            "* Se agrego la libreria de jdom 2.0.6 para manipular archivos xml" + "\n" +
-            "* Se agrego el menu de actualizacion" + "\n" +
-            "* Se corrigio bug para la conexio al servidor" + "\n" +
+            "* Se creo un nuevo splash" + "\n" +
+            "* Se modifico el color de la barra de menu y estados" + "\n" +
+            "* Se modifico el diseño de las ventanas principales" + "\n" +
+            "* Se modifico el diseño de la ventana principal" + "\n" +
+            "* Se modifico el reporte de cotización" + "\n" +
+            "* Se agrego iconos a los menús principales" + "\n" +
+            "* Se modifico la tabla de gestionar proyectos" + "\n" +
+            "* Se agrego mensajes de estado durante el proceso " + "\n" +
+            "~ solo para funciones principales en algunas ventanas" + "\n" +
+            "* Se modifico el reporte de cotización" + "\n" +
+            "* Se agrego nuevos componentes para capturar datos:" + "\n" +
+            "+ Componenten Teléfono de 10 dígitos" + "\n" +
+            "+ Componenten Datos Personales solo acepta caracteres" + "\n" +
+            "* Se elimino mensajes de estado en la barra de titulo" + "\n" +
+            "* Se corrigio bugs" + "\n" +
             "* etc." + "\n";
     
     public static final String Homepage = "https://www.myfreelab.com";
